77b88dac7cdddfacc72ce691d5bf9774bd28fee0
Heeryeong Park <heerpark@cx1r3s2.42seoul.kr>
2024-09-06 16:55:44 +0900

    d

--- inc/structures.h
+++ inc/structures.h
@@ -7,15 +7,20 @@ typedef struct s_vec3 t_color3;
 typedef struct s_ray t_ray;
 typedef struct s_camera t_camera;
 typedef struct s_canvas t_canvas;
-typedef struct s_object t_object;
+typedef struct  s_hit_record    t_hit_record;
+typedef struct  s_object    t_object;
 typedef struct s_sphere t_sphere;
+typedef struct s_light  t_light;
 typedef int             t_bool;
-typedef struct s_hit_record t_hit_record;
+typedef struct s_light  t_light;
+typedef struct s_scene	t_scene;
+typedef int             t_object_type;
 
 # define FALSE 0
 # define TRUE 1
-typedef int             t_object_type;
 # define SP 0
+# define LIGHT_POINT 1
+# define EPSILON 1e-6 // 0.000001
 
 struct s_vec3
 {
@@ -48,6 +53,30 @@ struct  s_canvas
     double  aspect_ratio; //종횡비
 };
 
+struct s_hit_record
+{
+    t_point3    p;
+    t_vec3      normal;
+    t_bool      front_face;
+    double      tmin;
+    double      tmax;
+    double      t;
+    /* * * * 추가 * * * */
+    t_color3    albedo;
+    /* * * * 추가 끝 * * * */
+};
+
+struct  s_scene
+{
+    t_canvas        canvas;
+    t_camera        camera;
+    t_object        *world;
+    t_object        *light;
+    t_color3         ambient; // 8.4에서 설명할 요소
+    t_ray           ray;
+    t_hit_record    rec;
+};
+
 struct  s_sphere
 {
 	t_point3    center;
@@ -55,21 +84,19 @@ struct  s_sphere
 	double      radius2;
 };
 
-struct s_hit_record
+struct      s_light
 {
-    t_point3    p;
-    t_vec3      normal;
-    double      tmin;
-    double      tmax;
-    double      t;
-    t_bool      front_face;
+    t_point3    origin;
+    t_color3    light_color;
+    double      bright_ratio;
 };
 
-struct                      s_object
+struct	s_object
 {
     t_object_type   type;
     void            *element;
     void            *next;
+	t_color3    albedo;
 };
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -50,7 +50,7 @@ int	main(void)
 		return (1);
     // 랜더링
     // P3 는 색상값이 아스키코드라는 뜻, 그리고 다음 줄은 캔버스의 가로, 세로 픽셀 수, 마지막은 사용할 색상값
-    printf("P3\n%d %d\n255\n", canv.width, canv.height);
+    printf("P3\n%d %d\n255\n", scene->canvas.width, scene->canvas.height);
     j = scene->canvas.height - 1;
     while (j >= 0)
     {
